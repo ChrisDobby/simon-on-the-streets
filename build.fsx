9f01868c0486142db5236ab550ab74e4a307bf2f
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-04-16 18:40:01 +0100

    Bumping version to 0.0.1

--- build.fsx
+++ build.fsx
@@ -1,8 +1,11 @@
 #r @"packages/build/FAKE/tools/FakeLib.dll"
 
 open System
+open Fake.ReleaseNotesHelper
+open System.IO
 
 open Fake
+open Fake.Git
 
 let serverPath = "./src/Server" |> FullName
 let clientPath = "./src/Client" |> FullName
@@ -90,6 +93,15 @@ Target "Bundle" (fun _ ->
   |> CopyFiles clientDir 
 )
 
+// Read additional information from the release notes document
+let releaseNotes = File.ReadAllLines "RELEASE_NOTES.md"
+
+let releaseNotesData =
+    releaseNotes
+    |> parseAllReleaseNotes
+
+let release = List.head releaseNotesData
+
 let dockerUser = getBuildParam "DockerUser"
 let dockerPassword = getBuildParam "DockerPassword"
 let dockerLoginServer = getBuildParam "DockerLoginServer"
@@ -97,41 +109,75 @@ let dockerImageName = getBuildParam "DockerImageName"
 
 let dockerFullName = sprintf "%s/%s" dockerUser dockerImageName
 
-printf "user: %s" dockerUser
-printf "password: %s" dockerPassword
-printf "server: %s" dockerLoginServer
-printf "image: %s" dockerImageName
-
-Target "Docker" (fun _ ->
-  let buildArgs = sprintf "build -t %s ." dockerFullName
-  run "docker" buildArgs "."
-
-  let tagArgs = sprintf "tag %s %s" dockerFullName dockerFullName
-  run "docker" tagArgs "."
+Target "SetReleaseNotes" (fun _ ->
+    let lines = [
+            "module internal ReleaseNotes"
+            ""
+            (sprintf "let Version = \"%s\"" release.NugetVersion)
+            ""
+            (sprintf "let IsPrerelease = %b" (release.SemVer.PreRelease <> None))
+            ""
+            "let Notes = \"\"\""] @ Array.toList releaseNotes @ ["\"\"\""]
+    File.WriteAllLines("src/Client/ReleaseNotes.fs",lines)
 )
 
-Target "Deploy" (fun _ ->
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- "docker"
-            info.WorkingDirectory <- deployDir
-            info.Arguments <- sprintf "login %s --username \"%s\" --password \"%s\"" dockerLoginServer dockerUser dockerPassword) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker login failed"
+Target "PrepareRelease" (fun _ ->
+    Git.Branches.checkout "" false "master"
+    Git.CommandHelper.directRunGitCommand "" "fetch origin" |> ignore
+    Git.CommandHelper.directRunGitCommand "" "fetch origin --tags" |> ignore
+
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bumping version to %O" release.NugetVersion)
+    Git.Branches.pushBranch "" "origin" "master"
+
+    let tagName = string release.NugetVersion
+    Git.Branches.tag "" tagName
+    Git.Branches.pushTag "" "origin" tagName
 
     let result =
         ExecProcess (fun info ->
             info.FileName <- "docker"
-            info.WorkingDirectory <- deployDir
-            info.Arguments <- sprintf "push %s/%s" dockerUser dockerImageName) TimeSpan.MaxValue
-    if result <> 0 then failwith "Docker push failed"
+            info.Arguments <- sprintf "tag %s/%s %s/%s:%s" dockerUser dockerImageName dockerUser dockerImageName release.NugetVersion) TimeSpan.MaxValue
+    if result <> 0 then failwith "Docker tag failed"
+)
+
+Target "CreateDockerImage" (fun _ ->
+    if String.IsNullOrEmpty dockerUser then
+        failwithf "docker username not given."
+    if String.IsNullOrEmpty dockerImageName then
+        failwithf "docker image Name not given."
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- "docker"
+            info.UseShellExecute <- false
+            info.Arguments <- sprintf "build -t %s/%s ." dockerUser dockerImageName) TimeSpan.MaxValue
+    if result <> 0 then failwith "Docker build failed"
+)
+
+Target "Deploy" (fun _ -> ()
+    // let result =
+    //     ExecProcess (fun info ->
+    //         info.FileName <- "docker"
+    //         info.WorkingDirectory <- deployDir
+    //         info.Arguments <- sprintf "login %s --username \"%s\" --password \"%s\"" dockerLoginServer dockerUser dockerPassword) TimeSpan.MaxValue
+    // if result <> 0 then failwith "Docker login failed"
+
+    // let result =
+    //     ExecProcess (fun info ->
+    //         info.FileName <- "docker"
+    //         info.WorkingDirectory <- deployDir
+    //         info.Arguments <- sprintf "push %s/%s" dockerUser dockerImageName) TimeSpan.MaxValue
+    // if result <> 0 then failwith "Docker push failed"
 )
 
 "Clean"
   ==> "InstallDotNetCore"
   ==> "InstallClient"
+  ==> "SetReleaseNotes"
   ==> "Build"
   ==> "Bundle"
-  ==> "Docker"
+  ==> "CreateDockerImage"
+  ==> "PrepareRelease"
   ==> "Deploy"
 
 "InstallClient"
---
+++ src/Client/ReleaseNotes.fs
@@ -0,0 +1,11 @@
+module internal ReleaseNotes
+
+let Version = "0.0.1"
+
+let IsPrerelease = false
+
+let Notes = """
+
+### 0.0.1 - 2018-04-16
+* First pre-release
+"""
